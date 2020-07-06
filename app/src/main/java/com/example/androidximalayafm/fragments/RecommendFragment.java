92c8bdb4b2d3ce3841677846af0c389710a9427b
zhangfan <zhangfan.makesail@bytedance.com>
2020-07-06 23:31:54 +0800

    喜马拉雅项目获取推荐内容数据

--- app/src/main/java/com/example/androidximalayafm/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/androidximalayafm/fragments/RecommendFragment.java
@@ -1,16 +1,65 @@
 package com.example.androidximalayafm.fragments;
 
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.RequiresApi;
+
 import com.example.androidximalayafm.R;
 import com.example.androidximalayafm.base.BaseFragment;
+import com.example.androidximalayafm.utils.Constants;
+import com.example.androidximalayafm.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class RecommendFragment  extends BaseFragment {
+    private static final String TAG = "RecommendFragment";
+
     @Override
     protected View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container) {
         View rootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
+        
+        
+        getRecommendData();
         return rootView;
+    }
+
+    /**
+     * 获取推荐内容，其实就是猜你喜欢
+     */
+    private void getRecommendData() {
+
+        Map<String,String> map = new HashMap<>();
+        // 这个参数表示一夜数据返回多少条
+        map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMEND_COUNT +"");
+        CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onSuccess(GussLikeAlbumList gussLikeAlbumList) {
+                // 数据获取成功
+                Optional.ofNullable(gussLikeAlbumList)
+                        .flatMap(s -> Optional.ofNullable(s.getAlbumList()))
+                        .ifPresent((albumList -> {
+                        LogUtil.d(TAG, "size --> " + albumList);
+                }));
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                // 获取获取失败
+                LogUtil.d(TAG, "error --> " + i);
+                LogUtil.d(TAG, "errorMsg -- >" + s);
+            }
+        });
     }
 }
