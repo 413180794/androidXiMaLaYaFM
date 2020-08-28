861db3df98e2528a69da60d600523989510994bd
zhangfan <zhangfan.makesail@bytedance.com>
2020-08-28 10:03:15 +0800

    34: 播放器的 UI 回调接口

--- app/src/main/java/com/example/androidximalayafm/interfaces/IPlayerCallback.java
+++ app/src/main/java/com/example/androidximalayafm/interfaces/IPlayerCallback.java
@@ -1,5 +1,11 @@
 package com.example.androidximalayafm.interfaces;
 
+
+import com.ximalaya.ting.android.opensdk.model.track.Track;
+import com.ximalaya.ting.android.opensdk.player.service.XmPlayListControl;
+
+import java.util.List;
+
 /**
  * IPlayerCallback
  *
@@ -10,5 +16,52 @@ package com.example.androidximalayafm.interfaces;
  * <p>
  * Description:
  */
-public interface IPlayerCallback {
+public interface IPlayerCallback{
+    /**
+     * 开始播放
+     */
+    void onPlayStart();
+
+    /**
+     * 播放暂停
+     */
+    void onPlayPause();
+
+
+    /**
+     * 播放停止
+     */
+    void onPlayStop();
+
+    /**
+     * 缓冲播放错误
+     */
+    void onPlayError();
+
+    /**
+     * 下一首
+     */
+    void onNextPlay(Track track);
+
+    /**
+     * 上一首
+     */
+    void onPrePlay(Track track);
+
+
+    // 播放列表数据加载完成
+    void onListLoaded(List<Track> list);
+
+    // 播放模式改变了
+    void onPlayModeChange(XmPlayListControl.PlayMode playMode);
+
+    // 进度条的改变
+    void onProgressChange(Long currentProgress, long totalProgress);
+
+    // 广告正在加载
+    void onAdLoading();
+
+    // 广告加载完成
+    void onAdFinished();
+
 }
