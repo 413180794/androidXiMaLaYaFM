73c693d409852f98e8eb2a79d33d1e6f19836992
zhangfan <zhangfan.makesail@bytedance.com>
2020-09-04 10:15:51 +0800

    36: 添加广告物料的监听

--- app/src/main/java/com/example/androidximalayafm/presenters/PlayerPresenter.java
+++ app/src/main/java/com/example/androidximalayafm/presenters/PlayerPresenter.java
@@ -4,8 +4,11 @@ import com.example.androidximalayafm.base.BaseApplication;
 import com.example.androidximalayafm.interfaces.IPlayerCallback;
 import com.example.androidximalayafm.interfaces.IPlayerPresenter;
 import com.example.androidximalayafm.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.model.advertis.Advertis;
+import com.ximalaya.ting.android.opensdk.model.advertis.AdvertisList;
 import com.ximalaya.ting.android.opensdk.model.track.Track;
 import com.ximalaya.ting.android.opensdk.player.XmPlayerManager;
+import com.ximalaya.ting.android.opensdk.player.advertis.IXmAdsStatusListener;
 import com.ximalaya.ting.android.opensdk.player.service.XmPlayListControl;
 
 import java.util.List;
@@ -20,14 +23,58 @@ import java.util.List;
  * <p>
  * Description:
  */
-public class PlayerPresenter implements IPlayerPresenter {
+public class PlayerPresenter implements IPlayerPresenter, IXmAdsStatusListener {
 
     private static final String TAG = "PlayerPresenter";
     private XmPlayerManager mPlayerManager;
 
     private PlayerPresenter() {
         mPlayerManager = XmPlayerManager.getInstance(BaseApplication.getAppContext());
+        mPlayerManager.addAdsStatusListener(this);
     }
+
+    /*=================广告相关的回调方法===================*/
+    @Override
+    public void onStartGetAdsInfo() {
+        LogUtil.d(TAG, " onStartGetAdsInfo");
+    }
+
+    @Override
+    public void onGetAdsInfo(AdvertisList advertisList) {
+
+        LogUtil.d(TAG, "  onGetAdsInfo ");
+    }
+
+    @Override
+    public void onAdsStartBuffering() {
+
+        LogUtil.d(TAG, " onAdsStartBuffering");
+    }
+
+    @Override
+    public void onAdsStopBuffering() {
+
+        LogUtil.d(TAG, " onAdsStopBuffering");
+    }
+
+    @Override
+    public void onStartPlayAds(Advertis advertis, int i) {
+
+        LogUtil.d(TAG, " onStartPlayAds");
+    }
+
+    @Override
+    public void onCompletePlayAds() {
+
+        LogUtil.d(TAG, " onCompletePlayAds");
+    }
+
+    @Override
+    public void onError(int what, int extra) {
+
+        LogUtil.d(TAG, " onError what =>" + what + " extra => " + extra);
+    }
+    /*=================广告相关的回调方法===================*/
 
     private static class PlayPresenterHolder {
         private static PlayerPresenter INSTANCE = new PlayerPresenter();
